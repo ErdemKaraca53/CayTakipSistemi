24cad2a39d565a31f84e713dee4d793b3f242647
ErdemKaraca53 <erdemkaraca5328@gmail.com>
2025-02-10 20:48:22 +0300

    recyclerviewda bir elaman seçiliyor sadece. yeni eleman seçilince eski elamanın işareti kalkıyor

--- app/src/main/java/com/erdem/designexample/adapter/BahceAdapter.kt
+++ app/src/main/java/com/erdem/designexample/adapter/BahceAdapter.kt
@@ -1,19 +1,26 @@
 package com.erdem.designexample.adapter
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.erdem.designexample.R
 
 class BahceAdapter(val dataSet: ArrayList<String>) : RecyclerView.Adapter<BahceAdapter.ViewHolder>() {
 
+    private var lastSelectPosition: Int = -1
+
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView
+        val card: CardView
 
         init {
             textView = view.findViewById(R.id.BahceRecyclerViewTextView)
+            card = view.findViewById(R.id.BahceRecyclerViewCardView)
         }
     }
 
@@ -25,9 +32,28 @@ class BahceAdapter(val dataSet: ArrayList<String>) : RecyclerView.Adapter<BahceA
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-
         viewHolder.textView.text = dataSet[position]
+
+        // Seçili elemanı kontrol edip arka plan rengini değiştir
+        if (position == lastSelectPosition) {
+            viewHolder.card.setBackgroundColor(ContextCompat.getColor(viewHolder.itemView.context, R.color.spinnerColor))
+            Log.e("bilgi", "Yeni seçim değişildi")
+        } else {
+            viewHolder.card.setBackgroundColor(ContextCompat.getColor(viewHolder.itemView.context, R.color.white))
+            Log.e("bilgi", "Eski seçim değişildi")
+        }
+
+        viewHolder.card.setOnClickListener {
+            val previousPosition = lastSelectPosition
+            lastSelectPosition = viewHolder.adapterPosition
+            Log.e("bilgi", "Tıklanıldı")
+            // Önceki seçili elemanı güncelle
+            notifyItemChanged(previousPosition)
+            // Yeni seçili elemanı güncelle
+            notifyItemChanged(lastSelectPosition)
+        }
     }
+
 
     // Return the size of your dataset (invoked by the layout manager)
     override fun getItemCount() = dataSet.size
