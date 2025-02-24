161ae66ebd9e6723bafdcec8e79915b03dcf5e67
ErdemKaraca53 <erdemkaraca5328@gmail.com>
2025-02-24 15:18:29 +0300

    .

--- app/src/main/java/com/erdem/designexample/adapter/RaporCardAdapter.kt
+++ app/src/main/java/com/erdem/designexample/adapter/RaporCardAdapter.kt
@@ -3,16 +3,17 @@ package com.erdem.designexample.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.erdem.designexample.R
+import androidx.core.view.isGone
 
 class RaporCardAdapter (private val dataSet: ArrayList<String>) :
     RecyclerView.Adapter<RaporCardAdapter.ViewHolder>() {
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView
-
         init {
             // Define click listener for the ViewHolder's View
             textView = view.findViewById(R.id.RaporRecyclerViewTextView)
@@ -29,6 +30,8 @@ class RaporCardAdapter (private val dataSet: ArrayList<String>) :
     // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         viewHolder.textView.text = dataSet[position]
+        viewHolder.textView.setOnClickListener {
+        }
     }
 
     override fun getItemCount() = dataSet.size
